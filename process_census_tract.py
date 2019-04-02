6d0e65e9018e91e78b9b4c4599ef4ba2b35db2be
Luke Shulman <lshulman@algorexhealth.com>
2019-04-02 11:36:50 -0400

    download then extract

--- process_census_tract.py
+++ process_census_tract.py
@@ -11,7 +11,7 @@ def ftp_login():
     ftp = FTP('ftp2.census.gov')
     ftp.login()
     print("Logged into Census Succesfully")
-    ftp.cwd('geo/tiger/TIGER2017/TRACT/')
+    ftp.cwd('geo/tiger/TIGER2015/TRACT/')
 
 
 def get_filenames():
@@ -19,24 +19,28 @@ def get_filenames():
     return filenames
 
 def download_file(filename):
-    with tempfile.TemporaryDirectory() as temp_directory:
         zip_temp_file = tempfile.TemporaryFile()
         print(f"Downloading {filename}")
         ftp.retrbinary('RETR ' + filename, zip_temp_file.write)
         print(f'Success {filename}')
         zip_file = zipfile.ZipFile(zip_temp_file)
         zip_file.extractall(path=temp_directory)
+        zip_temp_file.close()
+
+def extract_shape_files():
         shape_files = Path(temp_directory).glob('*.shp')
         for shape in shape_files:
-            print(f"Creating GeoJSON for {filename}")
-            geo_json_contents = gpd.read_file(shape).to_crs(crs).to_json()
-            with open(Path(Path.cwd(),'data','census_tract_shapes',shape.stem + '.json'), 'w') as geojson_file:
+                print(f"Creating GeoJSON for {filename}")
+                geo_json_contents = gpd.read_file(shape).to_crs(crs).to_json()
+                with open(Path(Path.cwd(),'data','census_tract_shapes',shape.stem + '.json'), 'w') as geojson_file:
                 geojson_file.write(geo_json_contents)
-        zip_temp_file.close()
+        
 
 
 ftp_login()
 filenames = get_filenames()
+with tempfile.TemporaryDirectory() as temp_directory:
 for ftp_file in filenames:
     download_file(ftp_file)
 ftp.quit()
+extract_shape_files()
